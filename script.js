155cd49cc6b44429a1c49774c14be5893e463680
marcioliveira <marcioruelaoliveira@gmail.com>
2021-07-25 20:41:11 -0300

    acessando a dom

--- script.js
+++
@@ -1,8 +0,0 @@
-const nav = document.getElementById("nav");
-alert(nav);
-
-let button = document.getElementById("changeBackground");
-
-button.addEventListener("click", () => {
-  document.body.style.backgroundColor = "fuchsia";
-});
